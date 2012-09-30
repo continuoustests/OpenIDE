7fccfa2776021ca0b64bcd14cb8542ed3c006baf
acken <acken@acken.no>
2012-10-01 01:11:42 +0200

    Added support for spesifying solution name in openide config

--- Languages/CSharp/initialize.sh
+++ Languages/CSharp/initialize.sh
@@ -4,6 +4,12 @@ ROOT=$(cd $(dirname "$0"); pwd)
 WORKING_DIR="$1"
 DEFAULT_LANGUAGE="$2"
 ENABLED_LANGUAGES="$3"
+SLN="$(oi configure read autotest.solution)"
 
-#oi process-start $ROOT/bin/AutoTest.Net/AutoTest.WinForms.exe "$WORKING_DIR"
-oi process-start $ROOT/bin/ContinuousTests/ContinuousTests.exe "$WORKING_DIR"
+WATCH_PATH="$WORKING_DIR"
+if [[ "$SLN" != "" ]]; then
+	WATCH_PATH=$WORKING_DIR/$SLN
+fi
+
+#oi process-start $ROOT/bin/AutoTest.Net/AutoTest.WinForms.exe "$WATCH_PATH"
+oi process-start $ROOT/bin/ContinuousTests/ContinuousTests.exe "$WATCH_PATH"
