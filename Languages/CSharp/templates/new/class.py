2ba6905eb4f14c0ff6dc72c610f43cef217d7350
acken <acken@acken.no>
2013-08-24 16:37:36 +0200

    Moved the rest of the scripts to python

---
+++ Languages/CSharp/templates/new/class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+
+if __name__ == "__main__":
+	if sys.argv[1] == 'get_file_extension':
+		print(".cs")
+	elif sys.argv[1] == 'get_position':
+		print("6|3")
+	elif sys.argv[1] == 'get_definition':
+		print("Creates an empty C# class")
+	else:
+		classname = sys.argv[1]
+		namespace = sys.argv[2]
+		parameterfile = sys.argv[3]
+		print("using System;")
+		print("")
+		print("namespace " + namespace)
+		print("{")
+		print("	class " + classname)
+		print("	{")
+		print("	}")
+		print("}")
