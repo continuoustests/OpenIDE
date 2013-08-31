6e24935c33493112cfb8790a0a34591b79410698
acken <acken@acken.no>
2013-09-01 00:07:44 +0200

    Fixed the php plugin. Now handles classes and namespaces

--- Languages/php/php.php
+++ Languages/php/php.php
@@ -1,5 +1,5 @@
 #!/usr/bin/env php
-<?php
+<?php namespace language_plugin;
 	if (count($argv) > 1) {
 		if ($argv[1] == "get-command-definitions")
 			handleDefinition();
@@ -21,6 +21,10 @@
 	}
 
 	function crawlSource($file) {
+		if (is_dir($file)) {
+			handleItem($file);
+			return;
+		}
 		$lines = file($file);
 		foreach ($lines as $line) {
 			handleItem(str_replace("\n", "", $line));
@@ -37,8 +41,10 @@
 				}
 			}
 		} else {
-			if (endsWith($item, ".php"))
-				handleFile($item);
+			if (endsWith($item, ".php")) {
+				$handler = new FileReader();
+				$handler->handleFile($item);
+			}
 		}
 	}
 
@@ -46,36 +52,103 @@
 		return (substr($str, strlen($str) - strlen($match)) === $match);
 	}
 
-	function handleFile($file) {
-		$first = TRUE;
-		$lineNum = 0;
-		$lines = file($file);
-		foreach ($lines as $line_str) {
-			$l = str_replace("\n", "", $line_str);
-            $lineNum++;
-            if (strstr(strtolower($l), "function") != FALSE)
-            {
-                $line = trim($l);
-                $paramPos = strpos($line, "(");
-                $namePos = strpos(strtolower($line), "function") + strlen("function") + 1;
-                if ($paramPos <= $namePos)
-                    continue;
-                if ($first) {
-                    write("file|" . $file . "|filesearch");
-                    $first = FALSE;
-                }
-                $name = substr($line, $namePos, $paramPos - $namePos);
-                $signature = $name;
-                $column = $namePos + 2;
-                write("signature||" .
+	class FileReader
+	{
+		private $namespace;
+
+		function __construct() {
+			$namespace = "";
+		}
+
+		public function handleFile($file) {
+			$lineNum = 0;
+			$lines = file($file);
+	        write("file|" . $file . "|filesearch");
+			foreach ($lines as $line_str) {
+	            $lineNum++;
+				$l = str_replace("\n", "", $line_str);
+	            $line = trim($l);
+	            if ($this->getNamespace($line, $lineNum))
+	            	continue;
+	            if ($this->getClass($line, $lineNum))
+	            	continue;
+			}
+		}
+
+		function getNamespace($line, $lineNum) {
+			if (strstr(strtolower($line), "namespace")) {
+				$item = $this->getKeyword("namespace", $line);
+				if ($item == NULL) {
+					return FALSE;
+				}
+				$signature = "";
+	            $column = $item->Column + 2;
+	            write("signature||" .
 					  $signature . "|" . 
-					  $name . 
-					  "|function||" . 
+					  $item->Name . 
+					  "|namespace||" . 
+					  $lineNum . "|" . 
+					  $column . 
+					  "||");
+	            $this->namespace = $item->Name;
+	            return TRUE;
+			}
+			return FALSE;
+		}
+
+		function getClass($line, $lineNum) {
+			if (strstr(strtolower($line), "class") != FALSE) {
+				$item = $this->getKeyword("class", $line);
+				if ($item == NULL) {
+					return FALSE;
+				}
+	            $signature = $item->Name;
+	            if ($this->namespace != "") {
+	            	$signature = $this->namespace . "." . $signature;
+	            }
+	            $column = $item->Column + 2;
+	            write("signature||" .
+					  $signature . "|" . 
+					  $item->Name . 
+					  "|class||" . 
 					  $lineNum . "|" . 
 					  $column . 
 					  "||typesearch");
+	            return TRUE;
+	        }
+	        return FALSE;
+		}
+
+		function getKeyword($type, $line) {
+			$paramPos = strpos($line, ":");
+            if ($paramPos === FALSE) {
+                $paramPos = strpos($line, "{");
+                if ($paramPos === FALSE) {
+                	$paramPos = strpos($line, ";");
+                	if ($paramPos === FALSE) {
+                		$paramPos = strlen($line);
+                	}
+                }
             }
-        }
+            $namePos = strpos(strtolower($line), $type) + strlen($type) + 1;
+            if ($paramPos <= $namePos)
+            	return NULL;
+            $name = trim(substr($line, $namePos, $paramPos - $namePos));
+            if (strstr($name, " ") || strstr($name, ";") || strstr($name, ")")) {
+            	return NULL;
+            }
+			return new Item($name, $namePos);
+		}
+	}
+
+	class Item {
+		public $Name = "";
+		public $Column = 0;
+
+		function __construct($name, $column) {
+			$this->Name = $name;
+			$this->Column = $column;
+		}
 	}
 
 	function write($s) {
